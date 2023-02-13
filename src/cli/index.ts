d6a065b15cd4c699c18d780f55b6d959c57018dd
1natsu <1natsu172@users.noreply.github.com>
2023-02-14 00:18:56 +0900

    cli

---
+++ src/cli/index.ts
@@ -0,0 +1,13 @@
+import { program } from '@commander-js/extra-typings'
+
+program
+  .name('ghp')
+  .version('0.1.0')
+  .command('install [name]', 'install one or more packages')
+  .command('search [query]', 'search with optional query')
+  .command('update', 'update installed packages', {
+    executableFile: 'myUpdateSubCommand',
+  })
+  .command('list', 'list packages installed', { isDefault: true })
+
+program.parse(process.argv)
--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,45 @@
 import { getEnv, octokit } from './libs'
+import './cli'
 
 console.log('is dry?', getEnv().DRY_RUN)
 
-const res = await octokit.graphql(
-  `{
-    nodes(ids: ["I_kwDOIqaxms5eAdRI"]) {
-      ... on Issue {
-        id
-        number
-        title
-        trackedInIssues(first: 10) {
-          nodes {
-            id
-            number
-            title
-            projectItems(first: 10) {
-              nodes {
-                ... on ProjectV2Item {
-                  fieldValues(first: 10) {
-                    nodes {
-                      ... on ProjectV2ItemFieldSingleSelectValue {
-                        field {
-                          ... on ProjectV2SingleSelectField {
-                            name
-                          }
-                        }
-                        name
-                        optionId
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }`,
-  // { login: 'octokit' },
-)
+// const res = await octokit.graphql(
+//   `{
+//     nodes(ids: ["I_kwDOIqaxms5eAdRI"]) {
+//       ... on Issue {
+//         id
+//         number
+//         title
+//         trackedInIssues(first: 10) {
+//           nodes {
+//             id
+//             number
+//             title
+//             projectItems(first: 10) {
+//               nodes {
+//                 ... on ProjectV2Item {
+//                   fieldValues(first: 10) {
+//                     nodes {
+//                       ... on ProjectV2ItemFieldSingleSelectValue {
+//                         field {
+//                           ... on ProjectV2SingleSelectField {
+//                             name
+//                           }
+//                         }
+//                         name
+//                         optionId
+//                       }
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }`,
+//   // { login: 'octokit' },
+// )
 
-console.log(res)
+// console.log(res)
